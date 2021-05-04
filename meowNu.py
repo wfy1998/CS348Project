126437fe7bd8b61680f1c1b81384e0a2e6e23b0a
wtyjoycee <42783113+wtyjoycee@users.noreply.github.com>
2021-05-05 00:34:08 +0800

    some updates

--- meowNu.py
+++ meowNu.py
@@ -1,8 +1,69 @@
 from dbconnect import connection
 import mysql.connector
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+import bcrypt
 
 app = Flask(__name__)
+session = {}
+@app.route('/')
+def home():
+    cnx, c = connection()
+    return render_template("home.html")
+
+@app.route('/register', methods = ["GET","POST"])
+def register():
+    cnx, c = connection()
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        name = request.form('name')
+        email = request.form('email')
+        password = request.form['password'].encode("utf-8")
+        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        query = "INSERT INTO user(username, email, password) VALUES " \
+                 "("+str(name)+","+str(email)+","+str(password)+");"
+        c.execute(query)
+        cnx.commit()
+                 
+        session['name'] = name
+        session['email'] = email
+        return render_template("login.html")
+
+@app.route('/login', methods = ["GET","POST"])
+def login():
+    cnx, c = connection()
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+
+        email = request.form('email')
+        password = request.form['password'].encode("utf-8")
+       
+        query = "select * from user where email = " + email+";"
+                 
+        c.execute(query)
+
+        user = c.fetchone()
+
+        if len(user) > 0:
+            if bcrypt.hashpw(password,user['password'].encode("utf-8")) == user['password'].encode("utf-8"):
+                session['name'] = name
+                session['email'] = email
+                return redirect(url_for("home"))
+            else:
+                return "ERROR"
+        else:
+            return "ERROR"
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
+
+@app.route('/profile')
+def profile():
+    
+    return render_template("profile.html")
+
 
 @app.route('/diet')
 def diet():
@@ -45,4 +106,4 @@ def addmeal():
         return redirect(url_for("diet"))
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
